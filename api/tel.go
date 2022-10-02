54d040f8be23d013ca862931a87f87c953a0dcb1
labulac <740162752@qq.com>
2022-10-02 13:11:05 +0800

    1

--- api/tel.go
+++ api/tel.go
@@ -3,8 +3,15 @@ package tel
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func Tel(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "123456")
+	fmt.Fprintf(w, "789")
+	os.Setenv("test", "1234")
+
+	a := os.Getenv("test")
+	fmt.Fprintf(w, a)
+
 }
